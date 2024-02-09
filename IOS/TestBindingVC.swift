14c26697d77de2f12c3733c5dc9c74d9779deabc
Ivan Sales Mendez <ivan.sales@enti.cat>
2024-02-09 20:23:04 +0100

    Added project

---
+++ IOS/TestBindingVC.swift
@@ -0,0 +1,22 @@
+//
+//  TestBindingVC.swift
+//  IOS
+//
+//  Created by Ivan Sales Mendez on 9/2/24.
+//
+
+import UIKit
+
+class TestBindingVC: UIViewController {
+
+    
+    @IBOutlet weak var redBox: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func HelloWorld(_ sender: Any) {
+    }
+}
